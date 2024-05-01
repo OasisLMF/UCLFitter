7d9d8d67cd6ecd043ac6f8f6ddbb86df72bb6edd
benhayes21 <ben.hayes@oasislmf.org>
2024-05-01 17:57:47 +0100

    footprint parts

--- model_data/sumatra/make_footprint.sh
+++ model_data/sumatra/make_footprint.sh
@@ -1,1 +1,1 @@
-cat footprint_parts/footprint_part_00 footprint_parts/footprint_part_01 footprint_parts/footprint_part_02 > footprint.bin.z
+cat footprint_parts/part00 footprint_parts/part01 footprint_parts/part02 footprint_parts/part03 footprint_parts/part04 footprint_parts/part05 > footprint.bin.z
