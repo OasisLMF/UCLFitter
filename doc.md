38f986a5fd886b7b6685b04706929be2a6d6a69f
Joh Carter <johanna.carter@oasislmf.org>
2024-09-25 17:54:14 +0100

    added vulnerability information

--- doc.md
+++ doc.md
@@ -8,7 +8,7 @@
 
 **Contact**: Serge Guillas, UCL s.guillas@ucl.ac.uk  
 
-# Hazard module 
+## Hazard module 
 
 **ID**: fitter_haz
 
@@ -16,18 +16,52 @@
 
 **Description**: Tsunami hazard footprint files (qualitative intensity) developed through statistical emulation for Java and Sumatra, Indonesia in Oasis format
 
-## Event set information 
+### Event set information 
 
 | Event Set | Type        | Calculation Method | Countries  | Number of events |
 |-----------|-------------|--------------------|------------|------------------|
 | fitter_haz_Jav | probabilistic | simulated | ['IDN'] | 1278 |
 | fitter_haz_Sum | probabilistic | simulated | ['IDN'] | 730 |
 
-## Hazard information 
+### Hazard information 
 
 | Event Set | Process types | Intensity measure | Trigger type  | Trigger process types |
 |-----------|---------------|-------------------|---------------|-----------------------|
 | fitter_haz_Jav | ['tsunami'] | DI:- | earthquake | ['ground_motion'] |
 | fitter_haz_Sum | ['tsunami'] | DI:- | earthquake | ['ground_motion'] |
+
+## Vulnerability module 
+
+**ID**: fitter_vln
+
+**Title**: FITTER household and business vulnerability data
+
+**Description**: Social vulnerability curves in Oasis format, relating qualitative levels of tsunami damage to percentage reduction in household and business asset value.
+
+### Hazard information 
+
+| Hazard type | Hazard process | Intensity |
+| tsunami | tsunami | DI:- |
+
+### Vulnerability functions 
+
+**Exposure category**: population
+
+**Countries**: ['IDN']
+
+**Impact type**: indirect
+
+**Impact metric**: economic_loss_value - household asset and business asset values
+
+**Impact unit**: percentage
+
+**Base data type**: observed
+
+**Approach**: empirical
+
+### Further details 
+
+The curves to estimate impact on household business assets and business recovery are developed using longitudinal household survey data (two waves of the STAR longitudinal survey carried out in Banda Aceh, Sumatra, 5–14 and 17–29 months after the 2004 Indian Ocean tsunami. See https://www.sciencedirect.com/science/article/pii/S2212420922000875
+
 
 This document has been generated using Risk Data Library Standard schema version 0.2. For more information about the RDLS please visit [RDLS](https://docs.riskdatalibrary.org/en/latest/)
--- gendoc.py
+++ gendoc.py
@@ -29,7 +29,7 @@ markdown += f"**License**: {dataset_license}\n\n"
 markdown += f"**Contact**: {contact_name} {contact_email}  \n\n"
 
 # Create a hazard module heading
-markdown+= "# Hazard module \n\n"
+markdown+= "## Hazard module \n\n"
 
 markdown += f"**ID**: {dataset_id}\n\n"
 markdown += f"**Title**: {dataset_title}\n\n"
@@ -37,7 +37,7 @@ markdown += f"**Description**: {dataset_description}\n\n"
 
 
 # Create a Markdown table
-markdown += "## Event set information \n\n"
+markdown += "### Event set information \n\n"
 markdown += "| Event Set | Type        | Calculation Method | Countries  | Number of events |\n"
 markdown += "|-----------|-------------|--------------------|------------|------------------|\n"
 
@@ -53,7 +53,7 @@ for event_set in event_sets:
     markdown += f"| {event_set_name} | {analysis_type} | {calculation_method} | {countries} | {event_count} |\n"
 
 markdown += "\n"
-markdown += "## Hazard information \n\n"
+markdown += "### Hazard information \n\n"
 markdown += "| Event Set | Process types | Intensity measure | Trigger type  | Trigger process types |\n"
 markdown += "|-----------|---------------|-------------------|---------------|-----------------------|\n"
 
@@ -69,6 +69,59 @@ for event_set in event_sets:
         trigger_processes = trigger.get('processes','N/A')
 
         markdown += f"| {hazards_type} | {hazards_processes} | {intensity_measure} | {trigger_type} | {trigger_processes} |\n"
+
+
+# Create a vulnerability module heading
+markdown+= "\n## Vulnerability module \n\n"
+
+datasetv = data.get('datasets', [])[2]  # Vulnerability
+
+# Get the root attributes
+dataset_id = datasetv.get('id', 'N/A')
+dataset_title = datasetv.get('title', 'N/A')
+dataset_description = datasetv.get('description', 'N/A')
+# Extract the vuln object from within the dataset
+vulnerability = datasetv.get('vulnerability', {})
+# Vuln attributes
+hazard_primary = vulnerability.get('hazard_primary','N/A')
+hazard_process_primary = vulnerability.get('hazard_process_primary','N/A') 
+intensity = vulnerability.get('intensity','N/A') 
+category = vulnerability.get('category','N/A') 
+analysis_details = vulnerability.get('analysis_details','N/A')
+
+impact = vulnerability.get('impact',{})
+impact_type = impact.get('type','N/A')
+impact_metric = impact.get('metric','N/A')
+impact_unit = impact.get('unit','N/A')
+impact_base_data_type = impact.get('base_data_type','N/A')
+
+spatial = vulnerability.get('spatial',{})
+countries = spatial.get('countries','N/A')
+
+functions = vulnerability.get('functions',{})
+vuln_function = functions.get('vulnerability',{})
+approach = vuln_function.get('approach','N/A')
+
+markdown += f"**ID**: {dataset_id}\n\n"
+markdown += f"**Title**: {dataset_title}\n\n"
+markdown += f"**Description**: {dataset_description}\n\n"
+
+markdown += "### Hazard information \n\n"
+markdown += "| Hazard type | Hazard process | Intensity |\n"
+markdown += f"| {hazard_primary} | {hazard_process_primary} | {intensity} |\n"
+
+markdown += "\n### Vulnerability functions \n\n"
+
+markdown += f"**Exposure category**: {category}\n\n"
+markdown += f"**Countries**: {countries}\n\n"
+markdown += f"**Impact type**: {impact_type}\n\n"
+markdown += f"**Impact metric**: {impact_metric}\n\n"
+markdown += f"**Impact unit**: {impact_unit}\n\n"
+markdown += f"**Base data type**: {impact_base_data_type}\n\n"
+markdown += f"**Approach**: {approach}\n\n"
+
+markdown += "### Further details \n\n"
+markdown += f"{analysis_details}\n\n"
 
 markdown += "\nThis document has been generated using Risk Data Library Standard schema version 0.2. For more information about the RDLS please visit [RDLS](https://docs.riskdatalibrary.org/en/latest/)"
 
